5bfb83dd75fb229a36297075acf3fce39778955f
George Barnick <george.barnick@brickcraft.me>
2013-12-01 15:09:04 -0500

    Update Donate.i18n.php

--- Donate/Donate.i18n.php
+++ Donate/Donate.i18n.php
@@ -11,8 +11,36 @@ $messages = array();
  * @author George Barnick
  */
 $messages[ 'en' ] = array(
-        'donate' => "Donate", // Important! This is the string that appears on Special:SpecialPages
+        'donate' => "Donate",
         'donate-desc' => "Custom donate extension developed by [[User:George (Brickimedia)|George]]. Exclusive to Brickimedia Association, and pays to [[User:Adrian (Brickimedia)|Adrian]]'s (server owner) PayPal account. Learn more about [[Donations]].",
+);
+
+/** German
+ * @author George Barnick
+ */
+$messages[ 'de' ] = array(
+        'donate' => "Spenden",
+);
+
+/** Spanish
+ * @author George Barnick
+ */
+$messages[ 'es' ] = array(
+        'donate' => "Donar",
+);
+
+/** Finnish
+ * @author George Barnick
+ */
+$messages[ 'fi' ] = array(
+        'donate' => "Lahjoittaa",
+);
+
+/** Danish
+ * @author George Barnick
+ */
+$messages[ 'dk' ] = array(
+        'donate' => "Doner",
 );
  
 /** Message documentation
