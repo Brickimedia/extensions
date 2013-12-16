b1e4ed6f0492d003aacd94dfef5ef93261d073f3
Cody <codyn329@users.noreply.github.com>
2013-12-15 20:14:51 -0600

    Authoring Vietnamese translation

--- RandomWiki/RandomWiki.i18n.php
+++ RandomWiki/RandomWiki.i18n.php
@@ -14,3 +14,8 @@ $messages['en'] = array(
 	'randomwiki' => 'Random Wiki', // Important! This is the string that appears on Special:SpecialPages
 	'randomwiki-desc' => 'Redirects the user to a random wiki on the [[m:Brickimedia Association|Brickimedia]] network',
 );
+
+$messages['vi'] = array(
+	'randomwiki' => 'Ngâu nhiên Wiki',
+	'randomwiki-desc' => 'Chuyển hướng người dùng đến một dự án ngẫu nhiên tại [[m:Brickimedia Association|Brickimedia]].',
+);
