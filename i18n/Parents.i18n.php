74e5fa15d94b22b05c5f082fb3a65c04d6e0235d
George Barnick <george.barnick@brickcraft.me>
2014-03-20 21:03:56 -0400

    Create Parents.i18n.php

---
+++ i18n/Parents.i18n.php
@@ -0,0 +1,10 @@
+<?php
+/* Brickipedia "Parents" footer listing */
+$messages = array();
+/** English
+ * @author George Barnick
+ */
+$messages['en'] = array(
+        'parents' => 'Parents',
+        'parentspage' => 'Project:Parents'
+);
