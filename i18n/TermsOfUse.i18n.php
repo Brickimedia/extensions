841648b518b61cf59af3ee5300b2a6d24d6e765a
George Barnick <george.barnick@brickcraft.me>
2014-03-21 14:56:23 -0400

    Update TermsOfUse.i18n.php

--- i18n/TermsOfUse.i18n.php
+++ i18n/TermsOfUse.i18n.php
@@ -5,47 +5,47 @@ $messages = array();
  */
 $messages['en'] = array(
         'termsofuse' => 'Terms of Use',
-        'termsofusepage' => 'm:Terms of Use'
+        'termsofusepage' => 'm:Special:MyLanguage/Terms of Use'
 );
 /** French/Français
  * @author George Barnick
  */
 $messages['fr'] = array(
         'termsofuse' => 'Conditions d\'utilisation',
-        'termsofusepage' => 'm:Terms of Use'
+        'termsofusepage' => 'm:Special:MyLanguage/Terms of Use'
 );
 /** German/Deutsch
  * @author George Barnick
  */
 $messages['de'] = array(
         'termsofuse' => 'Nutzungsbedingungen',
-        'termsofusepage' => 'm:Terms of Use'
+        'termsofusepage' => 'm:Special:MyLanguage/Terms of Use'
 );
 /** Polish/Polski
  * @author George Barnick
  */
 $messages['pl'] = array(
         'termsofuse' => 'Zasady użytkowania',
-        'termsofusepage' => 'm:Terms of Use'
+        'termsofusepage' => 'm:Special:MyLanguage/Terms of Use'
 );
 /** Spanish/Español
  * @author George Barnick
  */
 $messages['es'] = array(
         'termsofuse' => 'Términos de uso',
-        'termsofusepage' => 'm:Terms of Use'
+        'termsofusepage' => 'm:Special:MyLanguage/Terms of Use'
 );
 /** Vietnamese/Việt
  * @author Codyn329
  */
 $messages['vi'] = array(
         'termsofuse' => 'Điều khoản sử dụng',
-        'termsofusepage' => 'm:Terms of Use'
+        'termsofusepage' => 'm:Special:MyLanguage/Terms of Use'
 );
 /** Dutch (Nederlands)
  * @author George Barnick
  */
 $messages['nl'] = array(
         'termsofuse' => 'Gebruiksvoorwaarden',
-        'termsofusepage' => 'm:Terms of Use'
+        'termsofusepage' => 'm:Special:MyLanguage/Terms of Use'
 );
